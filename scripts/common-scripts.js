d622d381519263ce49a3d5aef2c6948b2022f3d7
Scott Henderson <henders999@gmail.com>
2017-04-28 19:55:14 -0700

    Added code to update any 'last-modified' elements with the document.lastModified property.

--- scripts/common-scripts.js
+++ scripts/common-scripts.js
@@ -16,6 +16,16 @@ var monthNames = [
 
 $(document).ready(function() {
   // Update any elements with the "current-month" id to the current month.
-  today = new Date();
-  document.getElementById("current-month").innerHTML = monthNames[today.getMonth()] + " " + today.getFullYear();
+  var today = new Date();
+  document.getElementById("current-month").innerHTML =
+    monthNames[today.getMonth()] + " " + today.getFullYear();
+
+  // Update any elements with the "last-modified" id to the last modified date of the document.
+  var lastModified = new Date(document.lastModified);
+  var year  = lastModified.getFullYear();
+  var month = lastModified.getMonth();
+  var day   = lastModified.getDate();
+
+  document.getElementById("last-modified").innerHTML =
+    "<small>Last updated on <time datetime='" + year + "-" + month + "-" + day + "'>" + monthNames[month] + " " + day + ", " + year + "</time></small>";
 });
