8d75fd56ee468fe565245a7e0c3645d765dbf448
Scott Henderson <henders999@gmail.com>
2017-05-18 20:45:05 -0700

    Modified to use jQuery instead of getElementById.

--- scripts/common-scripts.js
+++ scripts/common-scripts.js
@@ -15,10 +15,11 @@ var monthNames = [
 ];
 
 $(document).ready(function() {
+  
   // Update any elements with the "current-month" id to the current month.
   var today = new Date();
-  document.getElementById("current-month").innerHTML =
-    monthNames[today.getMonth()] + " " + today.getFullYear();
+  $("#current-month").text(
+    monthNames[today.getMonth()] + " " + today.getFullYear());
 
   // Update any elements with the "last-modified" id to the last modified date of the document.
   var lastModified = new Date(document.lastModified);
@@ -26,6 +27,7 @@ $(document).ready(function() {
   var month = lastModified.getMonth();
   var day   = lastModified.getDate();
 
-  document.getElementById("last-modified").innerHTML =
-    "<small>Last updated on <time datetime='" + year + "-" + month + "-" + day + "'>" + monthNames[month] + " " + day + ", " + year + "</time></small>";
+  $("#last-modified").html(
+    "<small>Last updated on <time datetime='" + year + "-" + month + "-" + day + "'>" +
+    monthNames[month] + " " + day + ", " + year + "</time></small>");
 });
