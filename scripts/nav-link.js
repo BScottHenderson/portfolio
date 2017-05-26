a145c590bde8ee1210704dd07a749e8e997abe5d
Scott Henderson <henders999@gmail.com>
2017-05-26 10:04:57 -0700

    Removed console logging for debugging.

--- scripts/nav-link.js
+++ scripts/nav-link.js
@@ -17,7 +17,6 @@ class NavLinks {
 
   createNavElement(currentPagePathName) {
 
-    console.log("createNavElement: currentPagePathName='" + currentPagePathName + "'");
     var nav = document.createElement("nav");
 
     // Add an anchor element for each link in the array.
@@ -34,7 +33,6 @@ class NavLinks {
 
 NavLinks.prototype.isRootDir = function(pathName) {
 
-  console.log("isRootDir: pathName='" + pathName + "'");
   var isRootDir = false;
 
   // If the path is just '/' then assume it's the root.
@@ -47,8 +45,6 @@ NavLinks.prototype.isRootDir = function(pathName) {
     var fileNameParts = pathName.split("/").pop().split(".");
     var fileNameNoExt = fileNameParts.slice(0, fileNameParts.length - 1).join("");
 
-    console.log("isRootDir: fileNameParts='" + fileNameParts + "'");
-    console.log("isRootDir: fileNameNoExt='" + fileNameNoExt + "'");
     if (fileNameNoExt === "index") {
       isRootDir = true;
     }
